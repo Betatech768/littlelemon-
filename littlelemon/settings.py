193b79d0d32584a77883dd1277302491c0b7d50f
Betatech768 <gudzy86@gmail.com>
2023-12-06 17:03:59 +0100

    testing the git push into the main git repository

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': 'chinexy009',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
